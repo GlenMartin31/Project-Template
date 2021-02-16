3d7c041dea08a2ea4ef8fb59fe2630ca9afe91aa
Glen Martin (home) <glen.martin@manchester.ac.uk>
2021-02-16 17:29:41 +0000

    Initial commit

---
+++ Scripts/01_Data_Cleaning.R
@@ -0,0 +1,10 @@
+# #######################################################################################################################
+
+# Author of code: Glen P. Martin.
+
+# INSERT SHORT DESCRIPTION OF WHAT THIS SCRIPT DOES
+
+# #######################################################################################################################
+
+#first script usually loads the data e.g. read.csv(here::here("Data", "Raw")) and applies data cleaning steps
+#Save any manipulated data in the folder here::here("Data", "Processed")
---
+++ Scripts/02_Analysis.R
@@ -0,0 +1,13 @@
+# #######################################################################################################################
+
+# Author of code: Glen P. Martin.
+
+# INSERT SHORT DESCRIPTION OF WHAT THIS SCRIPT DOES
+
+# #######################################################################################################################
+
+#second script usually calls the first script e.g. source(here::here("Scripts", "01_Data_Cleaning.R"))
+# and then might perform the first stage of the analysis (e.g. imputation, or model fitting etc.) 
+# Create additional R scripts as needed for each stage of the paper
+# Good practice is where each future script "calls" the previous (e.g. using source())
+
